ae06167c198d1c12dcbccbfbc7f70f14f2f1e743
kruno89 <kkukuc031@gmail.com>
2019-03-28 18:40:07 +0100

    popravljena drugametodasparamterima

--- HelloWorld/Controllers/PrviController.cs
+++ HelloWorld/Controllers/PrviController.cs
@@ -9,15 +9,19 @@ namespace HelloWorld.Controllers
     public class PrviController : Controller
     {
         // GET: Prvi/MetodaSParametrima/1349
-        public ActionResult MetodaSParametrima(int id)
+        public ActionResult MetodaSParametrima(int id=9431)
         {
             // return View();
             return Content (id.ToString());
         }
 
         // GET: Drugi/MetodaSParametrima/54321
-        public ViewResult DrugaMetodaSParametrima(int id)
+        public ViewResult DrugaMetodaSParametrima(int? id)
         {
+            if (id == null)
+            {
+                id = 777;
+            }
             return View((object)id);
             // return Content(id.ToString());
         }
