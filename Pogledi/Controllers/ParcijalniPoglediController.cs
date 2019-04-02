fd488f5a20c2f85b7cc85e4f5386569ea61d92e8
kruno89 <kkukuc031@gmail.com>
2019-04-02 18:29:28 +0200

    dodan primjer pogledi_partial_view, prirucnik,

---
+++ Pogledi/Controllers/ParcijalniPoglediController.cs
@@ -0,0 +1,24 @@
+﻿using Pogledi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pogledi.Controllers
+{
+    public class ParcijalniPoglediController : Controller
+    {
+        
+        public ViewResult PrikaziKosaricu()
+        {
+            List<Artikl> lArtikl = new List<Artikl>()
+            {
+                new Artikl () { Naziv="Kruh", Cijena=8.9m, Kolicina=3 },
+                new Artikl () { Naziv="Sir", Cijena=54.7m, Kolicina=1 },
+                new Artikl () { Naziv="Mlijeko", Cijena=6.5m, Kolicina=1 }
+            };
+            return View(lArtikl);
+        }
+    }
+}
