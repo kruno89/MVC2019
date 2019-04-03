321771bc2bb25d9fb0ebfa83f6aac42a2f49ba90
kruno89 <kkukuc031@gmail.com>
2019-04-03 16:53:59 +0200

    dodan child action i html helper textbox

---
+++ Pogledi/Controllers/ChildActionsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pogledi.Controllers
+{
+    public class ChildActionsController : Controller
+    {
+        // GET: ChildActions
+        public ViewResult ChildActionView()
+        {
+            string[] proizvodi = new string[] { "Banana", "Krumpir", "Brokula", "Mandarina",
+            "Limun", "Mrkva" };
+            return View(proizvodi);
+        }
+
+        [ChildActionOnly]
+        public string OdrediGrupuProizvoda(string proizvod)
+        {
+            switch (proizvod)
+            {
+                case "Banana":
+                case "Limun":
+                    return "Voće";
+                case "Mrkva":
+                case "Krumpir":
+                    return "Povrće";
+                default:
+                    return "Nepoznato";
+            }
+        }
+    }
+}
