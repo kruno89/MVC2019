1a5a374bf58132aada3f25d7170a61cb12850b6b
kruno89 <kkukuc031@gmail.com>
2019-04-03 18:38:58 +0200

    dodani primjeri za helper, do strane 74

---
+++ HtmlHelperi/Controllers/HtmlHelpersController.cs
@@ -0,0 +1,54 @@
+﻿using HtmlHelperi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelperi.Controllers
+{
+    public class HtmlHelpersController : Controller
+    {
+        // GET: HtmlHelpers
+        public ViewResult OsnovniHelperi()
+        {
+            return View();
+        }
+
+        // GET: PRVI ZAHTJEV POSLUŽITELJA
+        private string[] mjesta = new string[] { "Split", "Osijek", "Rijeka", "Zagreb" };
+
+        public ViewResult FormHelper()
+        {
+            ViewBag.Mjesta = mjesta;
+            return View(new Osoba());
+        }
+
+        // POST: Zahtjev
+        [HttpPost]
+        public ViewResult FormHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = mjesta;
+            ViewBag.Poruka = "Podaci uneseni!";
+            return View(osoba);
+        }
+
+        //GET: REQUEST
+        public ViewResult StrongTypedHelper()
+        {
+            ViewBag.Mjesta = mjesta;
+            return View(new Osoba());
+        }
+
+        //POST: REQUEST
+        [HttpPost]
+        public ViewResult StrongTypedHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = mjesta;
+            ViewBag.Poruka = "Podaci uneseni!";
+            return View(osoba);
+        }
+    }
+
+
+}
