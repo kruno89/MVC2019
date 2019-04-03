eff8b2fdcc8bfdf8f77e388a32bdb932b301dd60
kruno89 <kkukuc031@gmail.com>
2019-04-03 19:51:30 +0200

    dodan zadatak iz zbirke 5_5_1

---
+++ MVC projekt 5_5_vj/Controllers/PrikazSaChildAkcijomController.cs
@@ -0,0 +1,30 @@
+﻿using MVC_projekt_5_5_vj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_projekt_5_5_vj.Controllers
+{
+    public class PrikazSaChildAkcijomController : Controller
+    {
+        // GET: PrikazSaChildAkcijom
+        public ActionResult ObradiListu()
+        {
+            List<Artikal> artikli = new List<Artikal>()
+            {
+                new Artikal() { Naziv="Nogometna lopta", Cijena=250.60m, Kategorija="Sport" },
+                new Artikal() { Naziv="Električna gitara", Cijena=1890, Kategorija="Glazba" },
+                new Artikal() { Naziv="LED TV", Cijena=3560.99m, Kategorija="Tehnika" },
+            };
+            return View(artikli);
+        }
+
+        [ChildActionOnly]
+        public string OdrediKategoriju(Artikal artikal)
+        {
+            return artikal.Kategorija;
+        }
+    }
+}
---
+++ MVC projekt 5_5_vj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_projekt_5_5_vj.MvcApplication" Language="C#" %>
