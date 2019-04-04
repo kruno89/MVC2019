66dd7c5b6ed9c9c9b108be78968ee66f089682da
kruno89 <kkukuc031@gmail.com>
2019-04-04 17:08:16 +0200

    dodan primjer za pomocne metode po predlosku

---
+++ HtmlHelperi/Controllers/TemplHtmlHelperiController.cs
@@ -0,0 +1,36 @@
+﻿using HtmlHelperi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelperi.Controllers
+{
+    public class TemplHtmlHelperiController : Controller
+    {
+        // GET: TemplHtmlHelperi
+        public ViewResult HtmlEditorView()
+        {
+            return View(new OsobaTempl());
+        }
+
+        [HttpPost]
+        public ViewResult HtmlEditorView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+
+        //GET METODA
+        public ViewResult EditorModelView()
+        {
+            return View(new OsobaTempl());
+        }
+
+        [HttpPost]
+        public ViewResult EditorModelView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
