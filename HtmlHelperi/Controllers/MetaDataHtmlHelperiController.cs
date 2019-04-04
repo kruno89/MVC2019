adc98b4cb2b6bb79ce85d16ecfa835041cf08ff7
kruno89 <kkukuc031@gmail.com>
2019-04-04 18:33:23 +0200

    dodan primjer za korištenje metapodataka modela, obje verzije

---
+++ HtmlHelperi/Controllers/MetaDataHtmlHelperiController.cs
@@ -0,0 +1,35 @@
+﻿using HtmlHelperi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelperi.Controllers
+{
+    public class MetaDataHtmlHelperiController : Controller
+    {
+        // GET: MetaDataHtmlHelperi
+        public ViewResult MetaDataView()
+        {
+            return View(new OsobaMeta());
+        }
+        [HttpPost]
+        public ViewResult MetaDataView(OsobaMeta osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+
+        // GET // skraćena metoda
+        public ViewResult MetaDataView2()
+        {
+            return View(new OsobaMeta());
+        }
+
+        [HttpPost]
+        public ViewResult MetaDataView2(OsobaMeta osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
---
+++ HtmlHelperi/Models/OsobaMeta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace HtmlHelperi.Models
+{
+    [DisplayName("Osoba")]
+    public class OsobaMeta
+    {
+        [HiddenInput (DisplayValue =false)]
+        public int OsobaId { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+
+        [Display(Name="Mjesto stanovanja")]
+        [UIHint("TemplMjesta")]
+        public string MjestoStanovanja { get; set; }
+
+        [UIHint("TemplSpol")]
+        public string Spol { get; set; }
+
+        public bool Zaposlen { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DatumRodjenja { get; set; }
+
+        [UIHint("MultilineText")]
+        public string Napomena { get; set; }
+
+        public Adresa AdresaOsobe { get; set; }
+    }
+}
