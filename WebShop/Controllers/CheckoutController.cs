0a721c5cab0a9db3a855059673bb1dc3c14edc74
kruno89 <kkukuc031@gmail.com>
2019-04-12 18:28:49 +0200

    dodan drugi kontroler parcijalno

---
+++ WebShop/Controllers/CheckoutController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private WebShopEntities db = new WebShopEntities();
+
+
+        // GET: Checkout
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateUser([Bind(Include ="Id,Ime,Prezime,Email,AdresaDostave,Kontakt,Napomena")] Korisnici korisnici)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Korisnicis.Add(korisnici);
+                db.SaveChanges();
+                return RedirectToAction("CreateOrder", korisnici);
+            }
+
+            return View(korisnici);
+        }
+
+        public ActionResult CreateOrder(Korisnici korisnici)
+        {
+            Narudzbe narudzba = new Narudzbe();
+            narudzba.KorisnikId = korisnici.Id;
+            narudzba.DatumKreiranja = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+    }
+}
