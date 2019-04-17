2fde94c5fdc52f8ae3b8e119f56234bbc68cfca1
kruno89 <kkukuc031@gmail.com>
2019-04-17 20:42:51 +0200

    dodan parcijalni ispit, prvi zadatak

---
+++ Parcijalni_ispit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Parcijalni_ispit.MvcApplication" Language="C#" %>
---
+++ Parcijalni_ispit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parcijalni_ispit.Startup))]
+namespace Parcijalni_ispit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
