f9c4b000751ee0a8b8c00e6366a2b4a4cce2c173
kruno89 <kkukuc031@gmail.com>
2019-04-17 22:10:45 +0200

    dodan ispit mvc, prvi zadatak

---
+++ Ispit_MVC/Controllers/IspitController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ispit_MVC.Controllers
+{
+    public class IspitController : Controller
+    {
+        
+        public ViewResult Prvi()
+        {
+            ViewBag.Vrijeme = DateTime.Now;
+            return View();
+        }
+    }
+}
